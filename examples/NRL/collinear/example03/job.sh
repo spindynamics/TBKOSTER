955e39b361bdafc09f7369893b1b72a737dba06d
pthibaud <pthibaud@users.noreply.github.com>
2023-10-09 17:45:51 +0200

    plot bands from shell script

--- examples/NRL/collinear/example03/job.sh
+++ examples/NRL/collinear/example03/job.sh
@@ -116,6 +116,24 @@ cat > band/in_band.txt<<EOF
  /
 EOF
 
+cat > band/band_weight.gnuplot<<EOF
+set term png enh size 700,500
+set out 'band/projbands.png'
+#set xtics ("{/Symbol G}"0,"M"0.57735,"K"0.91068,"{/Symbol G}"1.57735)
+set xrange [*:*] ; set yrange [*:*]
+set grid xtics
+stats 'band/band_weight.dat'  u 1:2 nooutput
+set xra [STATS_min_x:STATS_max_x]
+set yra [STATS_min_y:STATS_max_y]
+set xlabel "k"
+set ylabel "E - E_F (eV)"
+set xzeroaxis
+set key opaque box width 1.0
+set style fill solid noborder
+radius(proj)=proj/200.
+plot 'band/band_weight.dat' u 1:2 lc rgb "grey" ,'band/band_weight.dat' u 1:2:(radius(\$3)) w circles lc rgb "red" t "{total}"
+EOF
+
 # Run TBKOSTER
 $BIN_DIR/TBKOSTER.x 
 
@@ -128,5 +146,5 @@ then
     $ECHO "The gnuplot command cound not be found. Please install gnuplot."
     exit 1
 else 
-    gnuplot $PREFIX/tools/band_weight.gnuplot
+    gnuplot band/band_weight.gnuplot
 fi
